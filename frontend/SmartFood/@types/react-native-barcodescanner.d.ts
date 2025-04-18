3fe942bd5fceb69772f1d8d9cad56bcb9921baca
Piotr Browarski <piotr@browarski.pl>
2025-04-18 21:27:04 +0200

    add frontend

---
+++ frontend/SmartFood/@types/react-native-barcodescanner.d.ts
@@ -0,0 +1,12 @@
+// declare module 'react-native-barcodescanner' {
+//     import { ComponentType } from 'react';
+//     import { StyleProp, ViewStyle } from 'react-native';
+
+//     export interface BarCodeScannerProps {
+//         onBarCodeScanned?: (data: any) => void;
+//         style?: StyleProp<ViewStyle>;
+//         barCodeTypes?: string[];
+//     }
+
+//     export const BarCodeScanner: ComponentType<BarCodeScannerProps>;
+// }
